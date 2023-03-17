4071ced254eb863d6cccaab4ef6129151a3c4246
Thiker <4900976+thiker@users.noreply.github.com>
2023-03-17 16:57:39 +0100

    update method for node presentation

--- Runtime/Scripts/GONode.cs
+++ Runtime/Scripts/GONode.cs
@@ -5,23 +5,42 @@ using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GONode : MonoBehaviour
 {
     private PTNode _nodeData;
+    private TextMeshPro _displayNameTMP;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject _displayNameObj = transform.Find("DisplayNameText").gameObject;
+        _displayNameTMP = _displayNameObj.GetComponent<TextMeshPro>();
+
+        // Set presentation
+        UpdateNodePresentation(_nodeData);
     }
 
     public void Init(PTNode nodeData)
     {
         _nodeData = nodeData;
-        _nodeData.PropertyChanged += (object obj, PropertyChangedEventArgs e) =>
+        _nodeData.PropertyChanged += OnNodeDataChanged;
+    }
+
+    private void OnNodeDataChanged(object obj, PropertyChangedEventArgs e)  
+    {
+        UpdateNodePresentation((PTNode)obj);
+    }
+
+    private void UpdateNodePresentation(PTNode nodeData)
+    {
+        if (nodeData == null)
         {
-            Debug.Log($"Changed event {e.PropertyName}");
-        };
+            return;
+        }
+
+        _displayNameTMP.text = nodeData?.DisplayName;
     }
 
     // Update is called once per frame
@@ -31,5 +50,6 @@ public class GONode : MonoBehaviour
 
     private void OnDestroy()
     {
+        _nodeData.PropertyChanged -= OnNodeDataChanged;
     }
 }
