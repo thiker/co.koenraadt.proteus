8d9a223da31b202ee3c53e64ca9ffbd0f5ae37ca
Thiker <4900976+thiker@users.noreply.github.com>
2023-06-29 18:23:31 +0200

    fixes for transform handle

--- Runtime/RuntimeTransformHandle/Scripts/Handles/Position/PositionHandle.cs
+++ Runtime/RuntimeTransformHandle/Scripts/Handles/Position/PositionHandle.cs
@@ -8,6 +8,9 @@ namespace RuntimeHandle
      */
     public class PositionHandle : MonoBehaviour
     {
+        //PROTEUS: ROOT FOR HANDLE
+        private GameObject _gizmoRoot;
+
         protected RuntimeTransformHandle _parentTransformHandle;
         protected List<PositionAxis> _axes;
         protected List<PositionPlane> _planes;
@@ -15,7 +18,8 @@ namespace RuntimeHandle
         public PositionHandle Initialize(RuntimeTransformHandle p_runtimeHandle)
         {
             _parentTransformHandle = p_runtimeHandle;
-            transform.SetParent(_parentTransformHandle.transform, false);
+            _gizmoRoot = _parentTransformHandle.transform.Find("GizmoRoot").gameObject;
+            transform.SetParent(_gizmoRoot.transform, false);
 
             _axes = new List<PositionAxis>();
 
@@ -23,18 +27,21 @@ namespace RuntimeHandle
                 GameObject o = new GameObject();
                 _axes.Add(o.AddComponent<PositionAxis>().Initialize(_parentTransformHandle, Vector3.right, Color.red));
                 o.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+                o.transform.SetParent(_gizmoRoot.transform);
             }
             
             if (_parentTransformHandle.axes == HandleAxes.Y || _parentTransformHandle.axes == HandleAxes.XY || _parentTransformHandle.axes == HandleAxes.YZ || _parentTransformHandle.axes == HandleAxes.XYZ) {
-                GameObject o = new GameObject();;
+                GameObject o = new GameObject();
                 _axes.Add(o.AddComponent<PositionAxis>().Initialize(_parentTransformHandle, Vector3.up, Color.green));
                 o.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+                o.transform.SetParent(_gizmoRoot.transform);
             }
 
             if (_parentTransformHandle.axes == HandleAxes.Z || _parentTransformHandle.axes == HandleAxes.XZ || _parentTransformHandle.axes == HandleAxes.YZ || _parentTransformHandle.axes == HandleAxes.XYZ) {
                 GameObject o = new GameObject();
                 _axes.Add(o.AddComponent<PositionAxis>().Initialize(_parentTransformHandle, Vector3.forward, Color.blue));
                 o.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+                o.transform.SetParent(_gizmoRoot.transform);
             }
 
             _planes = new List<PositionPlane>();
@@ -44,6 +51,7 @@ namespace RuntimeHandle
                 _planes.Add(o.AddComponent<PositionPlane>()
                     .Initialize(_parentTransformHandle, Vector3.right, Vector3.up, -Vector3.forward, new Color(0,0,1,.2f)));
                 o.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+                o.transform.SetParent(_gizmoRoot.transform);
             }
 
             if (_parentTransformHandle.axes == HandleAxes.YZ || _parentTransformHandle.axes == HandleAxes.XYZ) {
@@ -51,12 +59,14 @@ namespace RuntimeHandle
                 _planes.Add(o.AddComponent<PositionPlane>()
                     .Initialize(_parentTransformHandle, Vector3.up, Vector3.forward, Vector3.right, new Color(1, 0, 0, .2f)));
                 o.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+                o.transform.SetParent(_gizmoRoot.transform);
             }
             if (_parentTransformHandle.axes == HandleAxes.XZ || _parentTransformHandle.axes == HandleAxes.XYZ) {
                 GameObject o = new GameObject();
                 _planes.Add(o.AddComponent<PositionPlane>()
                     .Initialize(_parentTransformHandle, Vector3.right, Vector3.forward, Vector3.up, new Color(0, 1, 0, .2f)));
                 o.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+                o.transform.SetParent(_gizmoRoot.transform);
             }
 
             return this;
